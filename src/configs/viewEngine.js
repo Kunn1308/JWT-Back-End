93295e019dc2d02e5447b24ff1bf31d91a6f17f6
Minh Ngọc <ngockunn13801@gmail.com>
2024-05-22 17:56:55 +0700

    init project - hi world

---
+++ src/configs/viewEngine.js
@@ -0,0 +1,10 @@
+import express from "express";
+import path from "path";
+
+const configViewEngine = (app) => {
+    app.use(express.static(path.join("./src/public")));
+    app.set("view engine", "ejs");
+    app.set("views", path.join("./src", "views"));
+};
+
+export default configViewEngine;
