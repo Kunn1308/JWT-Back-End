90e7833a3b846bdef95572755294c2229179e572
Minh Ngọc <ngockunn13801@gmail.com>
2024-05-23 11:20:02 +0700

    video 5.1 - hash password user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,29 +1,16 @@
-import mysql from "mysql2/promise";
-
-// Create the connection to database
-const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    port: 3307,
-    password: "123456",
-});
+import userService from "../services/userService";
 
 const handleHelloWorld = (req, res) => {
     res.render("home");
 };
 
-const handleUserPage = (req, res) => {
+const handleUserPage = async (req, res) => {
     res.render("user");
 };
 
 const handleCreateNewUser = async (req, res) => {
     let { email, username, password } = req.body;
-    const [results, fields] = await connection.query(
-        "INSERT INTO users (email, username, password) VALUES (?,?,?)",
-        [email, username, password]
-    );
-    console.log(results);
+    await userService.createNewUser(email, username, password);
     res.send("create new user successfully");
 };
 
---
+++ src/services/userService.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import mysql from "mysql2/promise";
+
+// Create the connection to database
+const connection = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "jwt",
+    port: 3307,
+    password: "123456",
+});
+
+const salt = bcrypt.genSaltSync(10);
+
+const hashUserPassword = (password) => {
+    return bcrypt.hashSync(password, salt);
+};
+
+const createNewUser = async (email, username, password) => {
+    let hashPassword = hashUserPassword(password);
+    return await connection.query(
+        "INSERT INTO users (email, username, password) VALUES (?,?,?)",
+        [email, username, hashPassword]
+    );
+};
+
+const getUserList = async (req, res) => {
+    let users = [];
+    const [results, fields] = await connection.query("SELECT * FROM users");
+    console.log(results);
+};
+
+export default { createNewUser, getUserList };
