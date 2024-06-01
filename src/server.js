739a3f347653e655fb25c157c2421f74253402ba
Minh Ngọc <ngockunn13801@gmail.com>
2024-06-01 14:50:44 +0700

    video 9.4.3 - fix CORS

--- src/server.js
+++ src/server.js
@@ -6,6 +6,18 @@ require("dotenv").config();
 // import connection from "./configs/connectDB";
 
 const app = express();
+const PORT = process.env.PORT;
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.REACT_URL);
+    res.header("Access-Control-Allow-Headers", true);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
@@ -18,7 +30,6 @@ initWebRoutes(app);
 //test connection DB
 // connection();
 
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(">>> JWT backend is running on the port " + PORT);
 });
