43406f26c15a5f01e604e1f8de773846eb9862f5
Minh Ngọc <ngockunn13801@gmail.com>
2024-07-06 14:14:20 +0700

    video 12.5 - display/delete roles

--- src/controllers/roleController.js
+++ src/controllers/roleController.js
@@ -1,5 +1,22 @@
 import roleApiService from "../services/roleApiService";
-const showFunc = async (req, res) => {};
+const showFunc = async (req, res) => {
+    try {
+        let data = await roleApiService.getAllRoles();
+
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT,
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            EM: "error from server",
+            EC: -1,
+            DT: "",
+        });
+    }
+};
 const createFunc = async (req, res) => {
     try {
         let data = await roleApiService.createNewRoles(req.body);
@@ -19,7 +36,24 @@ const createFunc = async (req, res) => {
     }
 };
 const updateFunc = async (req, res) => {};
-const deleteFunc = async (req, res) => {};
+const deleteFunc = async (req, res) => {
+    try {
+        let data = await roleApiService.deleteRole(req.body.id);
+
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT,
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            EM: "error from server",
+            EC: -1,
+            DT: "",
+        });
+    }
+};
 export default {
     showFunc,
     createFunc,
--- src/services/roleApiService.js
+++ src/services/roleApiService.js
@@ -32,6 +32,50 @@ const createNewRoles = async (roles) => {
     }
 };
 
+const getAllRoles = async () => {
+    let role = await db.Role.findAll({
+        order: [["id", "DESC"]],
+    });
+    return {
+        EM: "Get all roles succeeds",
+        EC: 0,
+        DT: role,
+    };
+};
+
+const deleteRole = async (id) => {
+    try {
+        if (id) {
+            await db.Role.destroy({
+                where: {
+                    id,
+                },
+            });
+
+            return {
+                EM: "Delete Role is successfully",
+                EC: 0,
+                DT: [],
+            };
+        } else {
+            return {
+                EM: "Role is not found",
+                EC: 1,
+                DT: [],
+            };
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            EM: "Something wrong is service...",
+            EC: -2,
+            DT: "",
+        };
+    }
+};
+
 export default {
     createNewRoles,
+    getAllRoles,
+    deleteRole,
 };
